e9a5d1b6c110140272ec3a33ae5a668d31e4bdb4
Chris Miles <cmiles@obm.com>
2023-08-27 16:47:07 -0400

    multi form choice

--- src/RSVP.jsx
+++ src/RSVP.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   TextField,
@@ -14,22 +14,32 @@ function RSVP() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { isValid },
   } = useForm({
     mode: "onChange",
   });
 
+  const [numOfGuests, setNumOfGuests] = useState(1);
+
   const onSubmit = (data) => {
-    const guest = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      dinnerChoice: data.dinnerChoice,
-      songRecommendation: data.songRecommendation,
-      // Add a timestamp, RSVP date or any other info if you like
+    const guests = [];
+
+    for (let i = 0; i < numOfGuests; i++) {
+      const guest = {
+        firstName: data[`guest${i + 1}FirstName`],
+        lastName: data[`guest${i + 1}LastName`],
+        dinnerChoice: data[`guest${i + 1}DinnerChoice`],
+        songRecommendation: data[`guest${i + 1}SongRecommendation`],
+      };
+      guests.push(guest);
+    }
+
+    const payload = {
       rsvpDate: new Date().toISOString(),
+      guests,
     };
 
-    console.log(guest);
+    console.log(payload)
   };
 
   return (
@@ -37,55 +47,92 @@ function RSVP() {
       <Typography variant="h4" gutterBottom>
         RSVP
       </Typography>
+      <div
+        style={{
+          maxHeight: "80vh",
+          overflowY: "auto",
+          boxSizing: "border-box",
+          paddingRight: "20px",
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl variant="outlined" margin="normal" fullWidth>
+            <InputLabel>Number of Guests</InputLabel>
+            <Select
+              value={numOfGuests}
+              onChange={(e) => setNumOfGuests(e.target.value)}
+              label="Number of Guests"
+            >
+              {[1, 2, 3, 4].map((num) => (
+                <MenuItem key={num} value={num}>
+                  {num}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          label="First Name"
-          {...register("firstName", { required: "First Name is required" })}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-        />
+          {Array.from({ length: numOfGuests }).map((_, index) => (
+            <div key={index}>
+              <Typography variant="h6" gutterBottom>
+                Guest {index + 1}
+              </Typography>
 
-        <TextField
-          label="Last Name"
-          {...register("lastName", { required: "Last Name is required" })}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-        />
+              <TextField
+                label={`Guest ${index + 1} First Name`}
+                {...register(`guest${index + 1}FirstName`, {
+                  required: `First Name for Guest ${index + 1} is required`,
+                })}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+              />
 
-        <FormControl variant="outlined" margin="normal" fullWidth>
-          <InputLabel>Dinner Choice</InputLabel>
-          <Select
-            label="Dinner Choice"
-            {...register("dinnerChoice", {
-              required: "Dinner choice is required",
-            })}
+              <TextField
+                label={`Guest ${index + 1} Last Name`}
+                {...register(`guest${index + 1}LastName`, {
+                  required: `Last Name for Guest ${index + 1} is required`,
+                })}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+              />
+
+              <FormControl variant="outlined" margin="normal" fullWidth>
+                <InputLabel>Dinner Choice for Guest {index + 1}</InputLabel>
+                <Select
+                  label={`Dinner Choice for Guest ${index + 1}`}
+                  {...register(`guest${index + 1}DinnerChoice`, {
+                    required: `Dinner choice for Guest ${
+                      index + 1
+                    } is required`,
+                  })}
+                >
+                  <MenuItem value="chicken">Chicken</MenuItem>
+                  <MenuItem value="beef">Beef</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          ))}
+
+          <TextField
+            label="Song Recommendation"
+            {...register("songRecommendation")}
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ marginTop: "16px" }}
+            disabled={!isValid}
           >
-            <MenuItem value="chicken">Chicken</MenuItem>
-            <MenuItem value="beef">Beef</MenuItem>
-          </Select>
-        </FormControl>
-
-        <TextField
-          label="Song Recommendation"
-          {...register("songRecommendation")}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-        />
-
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          style={{ marginTop: "16px" }}
-          disabled={!isValid}
-        >
-          Submit
-        </Button>
-      </form>
+            Submit
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
